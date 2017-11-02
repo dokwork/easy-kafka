e444e121450d7e003ff6b0efd49363bd801002de
Vladimir Popov <vladimir@dokwork.ru>
2017-11-02 17:33:28 +0300

    updated KafkaProducer

--- src/test/scala/ru/dokwork/easy/kafka/KafkaProducerSpec.scala
+++ src/test/scala/ru/dokwork/easy/kafka/KafkaProducerSpec.scala
@@ -2,44 +2,38 @@ package ru.dokwork.easy.kafka
 
 import org.apache.kafka.clients.producer.{ Callback, Producer, ProducerRecord, RecordMetadata }
 import org.apache.kafka.common.TopicPartition
-import org.mockito.Matchers._
 import org.mockito.Mockito._
-import org.mockito.invocation.InvocationOnMock
 import org.scalatest.FreeSpec
 
-import scala.concurrent.{ Await, Awaitable }
 import scala.concurrent.duration._
+import scala.concurrent.{ Await, Awaitable }
 
 
-class KafkaProducerSpec extends FreeSpec {
+class KafkaProducerSpec extends FreeSpec with MockitoSugar {
+
+  type K = String
+  type V = String
 
   private def await(w: Awaitable[_]) = Await.result(w, 1.second)
 
-  "KafkaProducer" - {
-    "should invoke method o.a.k.clients.producer.Producer.send(ProducerRecord<K,V>, Callback)" in {
-      // given:
-      val javaProducer = mockAsyncKafkaProducer[String, String]()
-      val producer = new KafkaProducer[String, String](javaProducer)
+  private val stubMetadata = new RecordMetadata(new TopicPartition("", 0), 0, 0, 0, 0, 0, 0)
 
-      // when:
-      await(producer.send("topic", "Hello!"))
-
-      // then:
-      verify(javaProducer)
-        .send(any(classOf[ProducerRecord[String, String]]), any(classOf[Callback]))
-    }
+  trait Fixture {
+    val javaProducer = mock[Producer[K, V]]
+    when(javaProducer.send(any[ProducerRecord[K, V]], any[Callback]))
+      .thenAnswer((_: ProducerRecord[K, V], callback: Callback) => {
+        callback.onCompletion(stubMetadata, null)
+        mock[java.util.concurrent.Future[RecordMetadata]]
+      })
+    val producer = new KafkaProducer[K, V](javaProducer)
   }
 
-  private def mockAsyncKafkaProducer[K, V](): Producer[K, V] = {
-    val metadata = new RecordMetadata(new TopicPartition("", 0), 0, 0)
-    val producer = mock(classOf[Producer[K, V]])
-    doAnswer(
-      (invocation: InvocationOnMock) => {
-        val callback = invocation.getArguments.apply(1).asInstanceOf[Callback]
-        callback.onCompletion(metadata, null)
-        mock(classOf[java.util.concurrent.Future[RecordMetadata]])
-      }
-    ).when(producer).send(any(classOf[ProducerRecord[K, V]]), any(classOf[Callback]))
-    producer
+  "KafkaProducer" - {
+    "should invoke method send(ProducerRecord<K,V>, Callback)" in new Fixture {
+      // when:
+      await(producer.send("topic", "Hello!"))
+      // then:
+      verify(javaProducer).send(any[ProducerRecord[K, V]], any[Callback])
+    }
   }
 }
